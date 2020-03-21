d2873d1ec3f685d400d16e128f333f8b9c5d2b67
Victor <victorcwyu@gmail.com>
2020-03-21 12:49:27 -0400

    Created a selector, getAppointmentsForDay and implemented it using the Jest environment for testing.

---
+++ src/helpers/selectors.js
@@ -0,0 +1,15 @@
+export function getAppointmentsForDay(state, day) { 
+  const filteredDays = state.days.filter(dayOfDays => dayOfDays.name === day);
+  if (!day || filteredDays === [] || filteredDays[0] === undefined) {
+    return [];
+  };
+  const {appointments} = filteredDays[0];
+  const appointmentsArr = Object.values(state.appointments);
+  let result = [];
+    for (let item of appointmentsArr) {
+      if (appointments.includes(item.id)) {
+        result.push(item)
+      };
+    };
+    return result;
+};
