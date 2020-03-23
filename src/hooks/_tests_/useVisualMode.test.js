858f958e36c0bda0f8f584c5bc39db30285bca8c
Victor <victorcwyu@gmail.com>
2020-03-22 22:19:15 -0400

    Added third constant and associated tests.

--- src/hooks/_tests_/useVisualMode.test.js
+++ src/hooks/_tests_/useVisualMode.test.js
@@ -4,6 +4,7 @@ import { useVisualMode } from "hooks/useVisualMode";
 
 const FIRST = "FIRST";
 const SECOND = "SECOND";
+const THIRD = "THIRD";
 
 test("useVisualMode should initialize with default value", () => {
   const { result } = renderHook(() => useVisualMode(FIRST));
@@ -16,4 +17,20 @@ test("useVisualMode should transition to another mode", () => {
 
   act(() => result.current.transition(SECOND));
   expect(result.current.mode).toBe(SECOND);
+});
+
+test("useVisualMode should return to previous mode", () => {
+  const { result } = renderHook(() => useVisualMode(FIRST));
+
+  act(() => result.current.transition(SECOND));
+  expect(result.current.mode).toBe(SECOND);
+
+  act(() => result.current.transition(THIRD));
+  expect(result.current.mode).toBe(THIRD);
+
+  act(() => result.current.back());
+  expect(result.current.mode).toBe(SECOND);
+
+  act(() => result.current.back());
+  expect(result.current.mode).toBe(FIRST);
 });
