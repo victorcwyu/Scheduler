2b3f38d307ad8028612054d807dda83d04b356b4
Victor <victorcwyu@gmail.com>
2020-03-26 16:17:10 -0400

    Updated syntax in Application.test.js to use async/await.

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -6,9 +6,9 @@ import Application from "components/Application";
 
 afterEach(cleanup);
 
-it("defaults to Monday and changes the schedule when a new day is selected", () => {
+it("defaults to Monday and changes the schedule when a new day is selected", async() => {
   const { getByText } = render(<Application />);
-  return waitForElement(() => getByText("Monday")).then(() => {
+  await waitForElement(() => getByText("Monday")).then(() => {
     fireEvent.click(getByText("Tuesday"));
     expect(getByText("Leopold Silvers")).toBeInTheDocument();
   });
