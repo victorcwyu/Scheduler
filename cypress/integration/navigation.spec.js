81f14b0f145684c95b51c4bd1010def0206f04f2
Victor <victorcwyu@gmail.com>
2020-03-27 22:44:24 -0400

    Added first Cypress test to confirm that the user can switch between days of the week.

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -4,5 +4,8 @@ describe("Navigation", () => {
   });
   it("should navigate to Tuesday", () => {
     cy.visit("/");
+    cy.contains("[data-testid=day]", "Tuesday")
+      .click()
+      .should("have.class", "day-list__item--selected");
   });
 });
