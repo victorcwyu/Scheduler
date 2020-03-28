4136261815263c142c6f8aba1bff804cf02ca654
Victor <victorcwyu@gmail.com>
2020-03-28 15:46:06 -0400

    Completed writing appointments.spec.js file.  Now have working End-to End testing for booking, editing and cancelling interviews.

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,13 +1,37 @@
 describe("Appointments", () => {
-  it("should book an interview", () => {
-    cy.request("GET", "/api/debug/reset")
+  beforeEach(() => {
+    cy.request("GET", "/api/debug/reset");
     cy.visit("/");
     cy.contains("Monday");
+  });
+
+  xit("should book an interview", () => {
     cy.get("[alt=Add]")
       .first()
       .click();
-    // cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
-    // cy.get("[alt='Sylvia Palmer']").click();
-    // cy.contains("Save").click();
+    cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
+    cy.get('[alt="Sylvia Palmer"]').click();
+    cy.contains("Save").click();
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Sylvia Palmer");
+  });
+  xit("should edit an interview", () => {
+    cy.get("[alt=Edit]")
+      .first()
+      .click({ force: true });
+    cy.get("[data-testid=student-name-input]").clear().type("Lydia Miller-Jones");
+    cy.get('[alt="Tori Malcolm"]').click();
+    cy.contains("Save").click();
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Tori Malcolm");
+  });
+  it("should cancel an interview", () => {
+    cy.get("[alt=Delete]")
+      .first()
+      .click({ force: true });
+    cy.contains("Confirm").click();
+    cy.contains("Deleting").should("exist");
+    cy.contains("Deleting").should("not.exist");
+    cy.contains(".appointment__card--show", "Archie Cohen").should("not.exist");
   });
 });
